a7ce9175288bbd0b8ce9e41e8a449fe135cc641f
Jommys <jaime@example.com>
2025-05-23 13:35:33 +0200

    feat: man of steal

--- app/javascript/submission_form/completed.vue
+++ app/javascript/submission_form/completed.vue
@@ -5,22 +5,20 @@
     dir="auto"
   >
     <div class="font-medium text-2xl flex items-center space-x-1.5 mx-auto">
-      <IconCircleCheck
-        class="inline text-green-600"
-        :width="30"
-        :height="30"
-      />
+      <IconCircleCheck class="inline text-green-600" :width="30" :height="30" />
       <span class="completed-form-message-title">
-        {{ completedMessage.title || (hasSignatureFields ? (hasMultipleDocuments ? t('documents_have_been_signed') : t('document_has_been_signed')) : t('form_has_been_completed')) }}
+        {{
+          completedMessage.title ||
+          (hasSignatureFields
+            ? hasMultipleDocuments
+              ? t("documents_have_been_signed")
+              : t("document_has_been_signed")
+            : t("form_has_been_completed"))
+        }}
       </span>
     </div>
-    <div
-      v-if="completedMessage.body"
-      class="mt-2 completed-form-message-body"
-    >
-      <MarkdownContent
-        :string="completedMessage.body"
-      />
+    <div v-if="completedMessage.body" class="mt-2 completed-form-message-body">
+      <MarkdownContent :string="completedMessage.body" />
     </div>
     <div class="space-y-3 mt-5">
       <a
@@ -30,7 +28,7 @@
         class="white-button flex items-center w-full completed-form-completed-button"
       >
         <span>
-          {{ completedButton.title || 'Back to Website' }}
+          {{ completedButton.title || "Back to Website" }}
         </span>
       </a>
       <button
@@ -39,13 +37,10 @@
         :disabled="isSendingCopy"
         @click.prevent="sendCopyToEmail"
       >
-        <IconInnerShadowTop
-          v-if="isSendingCopy"
-          class="animate-spin"
-        />
+        <IconInnerShadowTop v-if="isSendingCopy" class="animate-spin" />
         <IconMail v-else />
         <span>
-          {{ t('send_copy_via_email') }}
+          {{ t("send_copy_via_email") }}
         </span>
       </button>
       <button
@@ -54,13 +49,10 @@
         :disabled="isDownloading"
         @click.prevent="download"
       >
-        <IconInnerShadowTop
-          v-if="isDownloading"
-          class="animate-spin"
-        />
+        <IconInnerShadowTop v-if="isDownloading" class="animate-spin" />
         <IconDownload v-else />
         <span>
-          {{ t('download') }}
+          {{ t("download") }}
         </span>
       </button>
       <a
@@ -70,9 +62,7 @@
         class="white-button flex items-center space-x-1 w-full"
       >
         <IconBrandGithub />
-        <span>
-          Star on Github
-        </span>
+        <span> Star on Github </span>
       </a>
       <a
         v-if="isDemo"
@@ -81,30 +71,26 @@
       >
         <IconLogin />
         <span>
-          {{ t('create_a_free_account') }}
+          {{ t("create_a_free_account") }}
         </span>
       </a>
-    </div>
-    <div
-      v-if="attribution"
-      class="text-center mt-4"
-    >
-      {{ t('powered_by') }}
-      <a
-        href="https://www.docuseal.com/start"
-        target="_blank"
-        class="underline"
-      >DocuSeal</a> - {{ t('open_source_documents_software') }}
     </div>
   </div>
 </template>
 
 <script>
-import { IconCircleCheck, IconBrandGithub, IconMail, IconDownload, IconInnerShadowTop, IconLogin } from '@tabler/icons-vue'
-import MarkdownContent from './markdown_content'
+import {
+  IconCircleCheck,
+  IconBrandGithub,
+  IconMail,
+  IconDownload,
+  IconInnerShadowTop,
+  IconLogin,
+} from "@tabler/icons-vue";
+import MarkdownContent from "./markdown_content";
 
 export default {
-  name: 'FormCompleted',
+  name: "FormCompleted",
   components: {
     MarkdownContent,
     IconCircleCheck,
@@ -112,177 +98,198 @@ export default {
     IconBrandGithub,
     IconMail,
     IconLogin,
-    IconDownload
+    IconDownload,
   },
-  inject: ['baseUrl', 't'],
+  inject: ["baseUrl", "t"],
   props: {
     submitterSlug: {
       type: String,
-      required: true
+      required: true,
     },
     isDemo: {
       type: Boolean,
       required: false,
-      default: false
+      default: false,
     },
     attribution: {
       type: Boolean,
       required: false,
-      default: true
+      default: true,
     },
     hasSignatureFields: {
       type: Boolean,
       required: false,
-      default: false
+      default: false,
     },
     hasMultipleDocuments: {
       type: Boolean,
       required: false,
-      default: false
+      default: false,
     },
     withDownloadButton: {
       type: Boolean,
       required: false,
-      default: true
+      default: true,
     },
     withSendCopyButton: {
       type: Boolean,
       required: false,
-      default: true
+      default: true,
     },
     withConfetti: {
       type: Boolean,
       required: false,
-      default: false
+      default: false,
     },
     canSendEmail: {
       type: Boolean,
       required: false,
-      default: false
+      default: false,
     },
     completedButton: {
       type: Object,
       required: false,
-      default: () => ({})
+      default: () => ({}),
     },
     completedMessage: {
       type: Object,
       required: false,
-      default: () => ({})
-    }
+      default: () => ({}),
+    },
   },
-  data () {
+  data() {
     return {
       isSendingCopy: false,
-      isDownloading: false
-    }
+      isDownloading: false,
+    };
   },
   computed: {
-    isWebView () {
-      return /webview|wv|ip((?!.*Safari)|(?=.*like Safari))/i.test(window.navigator.userAgent)
-    }
+    isWebView() {
+      return /webview|wv|ip((?!.*Safari)|(?=.*like Safari))/i.test(
+        window.navigator.userAgent
+      );
+    },
   },
-  async mounted () {
+  async mounted() {
     if (this.withConfetti) {
-      const { default: confetti } = await import('canvas-confetti')
+      const { default: confetti } = await import("canvas-confetti");
 
       confetti({
         particleCount: 50,
         startVelocity: 30,
-        spread: 140
-      })
+        spread: 140,
+      });
     }
 
-    document.querySelectorAll('#decline_button').forEach((button) => {
-      button.setAttribute('disabled', 'true')
-    })
+    document.querySelectorAll("#decline_button").forEach((button) => {
+      button.setAttribute("disabled", "true");
+    });
   },
   methods: {
-    sendCopyToEmail () {
-      this.isSendingCopy = true
+    sendCopyToEmail() {
+      this.isSendingCopy = true;
 
-      fetch(this.baseUrl + `/send_submission_email.json?submitter_slug=${this.submitterSlug}`, {
-        method: 'POST'
-      }).then(() => {
-        alert(this.t('email_has_been_sent'))
-      }).finally(() => {
-        this.isSendingCopy = false
-      })
-    },
-    download () {
-      this.isDownloading = true
-
-      fetch(this.baseUrl + `/submitters/${this.submitterSlug}/download`).then(async (response) => {
-        if (response.ok) {
-          const urls = await response.json()
-          const isMobileSafariIos = 'ontouchstart' in window && navigator.maxTouchPoints > 0 && /AppleWebKit/i.test(navigator.userAgent)
-          const isSafariIos = isMobileSafariIos || /iPhone|iPad|iPod/i.test(navigator.userAgent)
-
-          if (isSafariIos && urls.length > 1) {
-            this.downloadSafariIos(urls)
-          } else {
-            this.downloadUrls(urls)
-          }
-        } else {
-          alert(this.t('failed_to_download_files'))
+      fetch(
+        this.baseUrl +
+          `/send_submission_email.json?submitter_slug=${this.submitterSlug}`,
+        {
+          method: "POST",
         }
-      })
+      )
+        .then(() => {
+          alert(this.t("email_has_been_sent"));
+        })
+        .finally(() => {
+          this.isSendingCopy = false;
+        });
     },
-    downloadUrls (urls) {
+    download() {
+      this.isDownloading = true;
+
+      fetch(this.baseUrl + `/submitters/${this.submitterSlug}/download`).then(
+        async (response) => {
+          if (response.ok) {
+            const urls = await response.json();
+            const isMobileSafariIos =
+              "ontouchstart" in window &&
+              navigator.maxTouchPoints > 0 &&
+              /AppleWebKit/i.test(navigator.userAgent);
+            const isSafariIos =
+              isMobileSafariIos ||
+              /iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+            if (isSafariIos && urls.length > 1) {
+              this.downloadSafariIos(urls);
+            } else {
+              this.downloadUrls(urls);
+            }
+          } else {
+            alert(this.t("failed_to_download_files"));
+          }
+        }
+      );
+    },
+    downloadUrls(urls) {
       const fileRequests = urls.map((url) => {
         return () => {
           return fetch(url).then(async (resp) => {
-            const blobUrl = URL.createObjectURL(await resp.blob())
-            const link = document.createElement('a')
+            const blobUrl = URL.createObjectURL(await resp.blob());
+            const link = document.createElement("a");
 
-            link.href = blobUrl
-            link.setAttribute('download', decodeURI(url.split('/').pop()))
+            link.href = blobUrl;
+            link.setAttribute("download", decodeURI(url.split("/").pop()));
 
-            link.click()
+            link.click();
 
-            URL.revokeObjectURL(blobUrl)
-          })
-        }
-      })
+            URL.revokeObjectURL(blobUrl);
+          });
+        };
+      });
 
-      fileRequests.reduce(
-        (prevPromise, request) => prevPromise.then(() => request()),
-        Promise.resolve()
-      ).finally(() => {
-        this.isDownloading = false
-      })
+      fileRequests
+        .reduce(
+          (prevPromise, request) => prevPromise.then(() => request()),
+          Promise.resolve()
+        )
+        .finally(() => {
+          this.isDownloading = false;
+        });
     },
-    sanitizeHref (href) {
+    sanitizeHref(href) {
       if (href && href.trim().match(/^((?:https?:\/\/)|\/)/)) {
-        return href.replace(/javascript:/g, '')
+        return href.replace(/javascript:/g, "");
       }
     },
-    downloadSafariIos (urls) {
+    downloadSafariIos(urls) {
       const fileRequests = urls.map((url) => {
         return fetch(url).then(async (resp) => {
-          const blob = await resp.blob()
-          const blobUrl = URL.createObjectURL(blob.slice(0, blob.size, 'application/octet-stream'))
-          const link = document.createElement('a')
+          const blob = await resp.blob();
+          const blobUrl = URL.createObjectURL(
+            blob.slice(0, blob.size, "application/octet-stream")
+          );
+          const link = document.createElement("a");
 
-          link.href = blobUrl
-          link.setAttribute('download', decodeURI(url.split('/').pop()))
+          link.href = blobUrl;
+          link.setAttribute("download", decodeURI(url.split("/").pop()));
 
-          return link
+          return link;
+        });
+      });
+
+      Promise.all(fileRequests)
+        .then((links) => {
+          links.forEach((link, index) => {
+            setTimeout(() => {
+              link.click();
+
+              URL.revokeObjectURL(link.href);
+            }, index * 50);
+          });
         })
-      })
-
-      Promise.all(fileRequests).then((links) => {
-        links.forEach((link, index) => {
-          setTimeout(() => {
-            link.click()
-
-            URL.revokeObjectURL(link.href)
-          }, index * 50)
-        })
-      }).finally(() => {
-        this.isDownloading = false
-      })
-    }
-  }
-}
+        .finally(() => {
+          this.isDownloading = false;
+        });
+    },
+  },
+};
 </script>
